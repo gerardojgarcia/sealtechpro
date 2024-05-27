cfac78a72d303b5b404a812ab7807e9b5b2c0fea
Gerardo J Garcia <gerardojgarcia@pm.me>
2024-05-27 13:15:31 -0700

    macbook sync

--- components/TheServicesSection.vue
+++ components/TheServicesSection.vue
@@ -7,56 +7,48 @@
   >
     <div class="grid place-items-center rounded-lg" data-aos="fade-right">
       <img
-        src="/sealtech_3.webp"
+        src="/"
         alt="Image of men doig maintenance on solar panels"
         class="rounded-lg p-4 md:p-32"
         height="600px"
       />
     </div>
     <div class="p-8" data-aos="fade-left">
-      <h2 class="text-4xl mb-6">Crack and Joint Repair</h2>
+      <h2 class="text-4xl mb-6">Comprehensive Solar Panel Services</h2>
       <p class="text-2xl">
-        Repairing cracks and joints in your concrete is essential for long-lasting results. We meticulously repair these areas to ensure a smooth and even surface before applying the sealant.
+        Keep your solar panels performing at their peak with SolarSustain's expert
+        cleaning and maintenance services. Whether you're looking for a one-time clean or
+        ongoing care, we have flexible options tailored to your needs.
       </p>
       <div class="mt-8">
-        <strong class="text-2xl">Epoxy Sealing</strong>
+        <strong class="text-2xl">One-Time Cleaning Services</strong>
         <p class="p-8">
-          For a durable and glossy finish, epoxy sealing is the ideal choice. This high-performance sealer provides excellent protection and an attractive appearance, perfect for both residential and commercial properties.
+          Our one-time cleaning service is perfect for those looking for immediate
+          maintenance or a quick enhancement of their system's efficiency. Our pricing is
+          structured based on the number of panels, ensuring a cost-effective solution for
+          every size of installation.
         </p>
-        <strong class="text-2xl">Polyurethane Sealing</strong>
+        <strong class="text-2xl">Subscription-Based Cleaning (Bi-Annual):</strong>
         <p class="p-8">
-          Polyurethane sealing offers enhanced protection and durability. This sealer is perfect for areas that need a robust and long-lasting barrier against the elements.
+          Our one-time cleaning service is perfect for those looking for immediate
+          maintenance or a quick enhancement of their system's efficiency. Our pricing is
+          structured based on the number of panels, ensuring a cost-effective solution for
+          every size of installation.
         </p>
-        <strong class="text-2xl">Penetrating Sealing</strong>
+        <strong class="text-2xl">Our Promise</strong>
         <p class="p-8">
-          Protect your concrete from within with our penetrating sealers. These sealants seep deep into the concrete, offering superior protection against moisture and wear.
-        </p>
-        <strong class="text-2xl">Acrylic Sealing</strong>
-        <p class="p-8">
-          Acrylic sealing provides a cost-effective protective layer for your concrete surfaces. This sealer enhances the appearance of your concrete while offering reliable protection.
-        </p>
-        <strong class="text-2xl">Water-Based Sealing</strong>
-        <p class="p-8">
-          For an environmentally friendly option, consider our water-based sealers. These sealants provide excellent protection while being safer for the environment.
-        </p>
-        <strong class="text-2xl">Decorative Sealing (e.g., Staining)</strong>
-        <p class="p-8">
-          Enhance the aesthetic appeal of your concrete with our decorative sealing services. From staining to custom finishes, we add a unique touch while providing robust protection.
-        </p>
-        <strong class="text-2xl">Anti-Slip Treatment</strong>
-        <p class="p-8">
-          Safety is paramount. Our anti-slip treatments add traction to your concrete surfaces, reducing the risk of slips and falls, especially in wet conditions.
-        </p>
-        <strong class="text-2xl">Maintenance and Resealing</strong>
-        <p class="p-8">
-          Regular maintenance and resealing are essential to keep your concrete in top condition. We offer periodic resealing services to maintain the protective layer and extend the lifespan of your concrete surfaces.
+          For those committed to long-term efficiency, our subscription-based cleaning
+          service offers a convenient and cost-effective solution. Subscribe to our
+          bi-annual plan and receive a 10% discount on the total cost. This plan ensures
+          your solar panels are maintained regularly, which is key to maximizing energy
+          output and extending the lifespan of your system.
         </p>
       </div>
       <div class="button-container justify-between py-8 flex md:w-3/4">
         <NuxtLink
           to="quote"
           class="hero-quote bg-blue-400 p-2 rounded-md hover:bg-blue-300 dark:text-white hover:text-black duration-150"
-          >Get Your Free Quote</NuxtLink
+          >Book Your Cleaning Today</NuxtLink
         >
         <NuxtLink
           to="quote"
