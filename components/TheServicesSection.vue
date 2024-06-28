3eeda89234eb5e8c0c344f7cb31c5c100d47035e
Gerardo J Garcia <gerardojgarcia@pm.me>
2024-06-27 20:42:41 -0700

    updated climate impact blog

--- components/TheServicesSection.vue
+++ components/TheServicesSection.vue
@@ -1,98 +1,102 @@
 <script setup></script>
 
 <template>
-  <section
-    id="services"
-    class="grid md:grid-cols-2 place-items-center dark:text-white dark:bg-dark_slate_gray"
-  >
-    <div class="grid place-items-center rounded-lg" data-aos="fade-right">
-      <img
-        src="/sealtech_3.webp"
-        alt="Image of men doig maintenance on solar panels"
-        class="rounded-lg p-4 md:p-32"
-        height="600px"
-      />
-    </div>
-    <div class="p-8" data-aos="fade-left">
-      <h2 class="text-4xl mb-6">Our Services</h2>
-      <p class="text-2xl">
-        Before sealing, proper cleaning and preparation of the concrete surface are
-        crucial. Our team ensures that every surface is thoroughly cleaned and prepped,
-        providing a solid foundation for the sealing process.
-      </p>
-      <div class="mt-8 grid grid-cols-2">
-        <div>
-          <strong class="text-2xl">Surface Preparation</strong>
-          <p class="p-8">
-            Before sealing, proper cleaning and preparation of the concrete surface are
-            crucial. Our team ensures that every surface is thoroughly cleaned and
-            prepped, providing a solid foundation for the sealing process.
-          </p>
+    <section
+        id="services"
+        class="grid md:grid-cols-2 place-items-center dark:text-white dark:bg-dark_slate_gray"
+    >
+        <div class="grid place-items-center rounded-lg" data-aos="fade-right">
+            <img
+                src="/sealtech_3.webp"
+                alt="Image of men doig maintenance on solar panels"
+                class="rounded-lg p-4 md:p-32"
+                height="600px"
+            />
         </div>
-        <div>
-          <strong class="text-2xl">Crack and Joint Repair</strong>
-          <p class="p-8">
-            Repairing cracks and joints in your concrete is essential for long-lasting
-            results. We meticulously repair these areas to ensure a smooth and even
-            surface before applying the sealant.
-          </p>
+        <div class="p-8" data-aos="fade-left">
+            <h2 class="text-4xl mb-6">Our Services</h2>
+            <p class="text-2xl">
+                Before sealing, proper cleaning and preparation of the concrete
+                surface are crucial. Our team ensures that every surface is
+                thoroughly cleaned and prepped, providing a solid foundation for
+                the sealing process.
+            </p>
+            <div class="mt-8 grid grid-cols-2">
+                <div>
+                    <strong class="text-2xl">Surface Preparation</strong>
+                    <p class="p-8">
+                        Before sealing, proper cleaning and preparation of the
+                        concrete surface are crucial. Our team ensures that
+                        every surface is thoroughly cleaned and prepped,
+                        providing a solid foundation for the sealing process.
+                    </p>
+                </div>
+                <div>
+                    <strong class="text-2xl">Crack and Joint Repair</strong>
+                    <p class="p-8">
+                        Repairing cracks and joints in your concrete is
+                        essential for long-lasting results. We meticulously
+                        repair these areas to ensure a smooth and even surface
+                        before applying the sealant.
+                    </p>
+                </div>
+                <div>
+                    <strong class="text-2xl">Epoxy Sealing</strong>
+                    <p class="p-8">
+                        For a durable and glossy finish, epoxy sealing is the
+                        ideal choice. This high-performance sealer provides
+                        excellent protection and an attractive appearance,
+                        perfect for both residential and commercial properties.
+                    </p>
+                </div>
+                <div>
+                    <strong class="text-2xl">Polyurethane Sealing</strong>
+                    <p class="p-8">
+                        Polyurethane sealing offers enhanced protection and
+                        durability. This sealer is perfect for areas that need a
+                        robust and long-lasting barrier against the elements.
+                    </p>
+                </div>
+                <div>
+                    <strong class="text-2xl">Penetrating Sealing</strong>
+                    <p class="p-8">
+                        Polyurethane sealing offers enhanced protection and
+                        durability. This sealer is perfect for areas that need a
+                        robust and long-lasting barrier against the elements.
+                    </p>
+                </div>
+                <div>
+                    <strong class="text-2xl">Polyurethane Sealing</strong>
+                    <p class="p-8">
+                        Polyurethane sealing offers enhanced protection and
+                        durability. This sealer is perfect for areas that need a
+                        robust and long-lasting barrier against the elements.
+                    </p>
+                </div>
+                <div>
+                    <strong class="text-2xl">Polyurethane Sealing</strong>
+                    <p class="p-8">
+                        Polyurethane sealing offers enhanced protection and
+                        durability. This sealer is perfect for areas that need a
+                        robust and long-lasting barrier against the elements.
+                    </p>
+                </div>
+                <div>
+                    <strong class="text-2xl">Polyurethane Sealing</strong>
+                    <p class="p-8">
+                        Polyurethane sealing offers enhanced protection and
+                        durability. This sealer is perfect for areas that need a
+                        robust and long-lasting barrier against the elements.
+                    </p>
+                </div>
+            </div>
+            <div class="button-container justify-between py-8 flex md:w-3/4">
+                <NuxtLink
+                    to="quote"
+                    class="hero-quote border-safety_orange border-2 p-2 rounded-md hover:bg-safety_orange text-white duration-150"
+                    >Book Your Sealing Today</NuxtLink
+                >
+            </div>
         </div>
-        <div>
-          <strong class="text-2xl">Epoxy Sealing</strong>
-          <p class="p-8">
-            For a durable and glossy finish, epoxy sealing is the ideal choice. This
-            high-performance sealer provides excellent protection and an attractive
-            appearance, perfect for both residential and commercial properties.
-          </p>
-        </div>
-        <div>
-          <strong class="text-2xl">Polyurethane Sealing</strong>
-          <p class="p-8">
-            Polyurethane sealing offers enhanced protection and durability. This sealer is
-            perfect for areas that need a robust and long-lasting barrier against the
-            elements.
-          </p>
-        </div>
-        <div>
-          <strong class="text-2xl">Penetrating Sealing</strong>
-          <p class="p-8">
-            Polyurethane sealing offers enhanced protection and durability. This sealer is
-            perfect for areas that need a robust and long-lasting barrier against the
-            elements.
-          </p>
-        </div>
-        <div>
-          <strong class="text-2xl">Polyurethane Sealing</strong>
-          <p class="p-8">
-            Polyurethane sealing offers enhanced protection and durability. This sealer is
-            perfect for areas that need a robust and long-lasting barrier against the
-            elements.
-          </p>
-        </div>
-        <div>
-          <strong class="text-2xl">Polyurethane Sealing</strong>
-          <p class="p-8">
-            Polyurethane sealing offers enhanced protection and durability. This sealer is
-            perfect for areas that need a robust and long-lasting barrier against the
-            elements.
-          </p>
-        </div>
-        <div>
-          <strong class="text-2xl">Polyurethane Sealing</strong>
-          <p class="p-8">
-            Polyurethane sealing offers enhanced protection and durability. This sealer is
-            perfect for areas that need a robust and long-lasting barrier against the
-            elements.
-          </p>
-        </div>
-      </div>
-      <div class="button-container justify-between py-8 flex md:w-3/4">
-        <NuxtLink
-          to="quote"
-          class="hero-quote border-safety_orange border-2 p-2 rounded-md hover:bg-safety_orange hover:text-white text-black duration-150"
-          >Book Your Sealing Today</NuxtLink
-        >
-      </div>
-    </div>
-  </section>
+    </section>
 </template>
